55641366c5b6ada8b81841b80528d8abcd7ec561
github username <github email address>
2018-12-19 14:44:42 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+# returns a new collection of appropriately modified elements,
+# in this case capitalized languages
 
 
+def my_collect(array)
+  i = 0
+  new_arr = []
+  while i < array.length
+    new_arr << yield(array[i])
+    i += 1
+  end
+  new_arr
+end
